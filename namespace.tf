e8da0176784f5832cc7d2e116d5f9feb7e9da999
Yevgeni Shapiro <23049337+yevgenis2006@users.noreply.github.com>
2026-01-03 22:51:42 +0200

    update

--- namespace.tf
+++ namespace.tf
@@ -1,7 +1,7 @@
 
-resource "kubernetes_namespace" "crossplane" {
+resource "kubernetes_namespace" "crossplane_system" {
 metadata {
-name = "crossplane"
+name = "crossplane-system"
 labels = {
 "app.kubernetes.io/managed-by" = "terraform"
   }
