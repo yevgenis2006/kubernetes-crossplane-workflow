8855ff7a401729583f4a06891e6ad07119f98fee
Yevgeni Shapiro <23049337+yevgenis2006@users.noreply.github.com>
2026-01-03 23:47:34 +0200

    update

--- config/gcp/instance.tf
+++ config/gcp/instance.tf
@@ -49,3 +49,10 @@ variable "gcp_sa_key_file" {
   description = "Path to GCP Service Account JSON"
 }
 
+variable "gcp_project" {}
+variable "gcp_zone" {
+  default = "us-central1-a"
+}
+variable "gcp_network" {
+  default = "default"
+}
