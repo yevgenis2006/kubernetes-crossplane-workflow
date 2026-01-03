79fecd97ea816ba998fccef287b92197cf7c21a3
Yevgeni Shapiro <23049337+yevgenis2006@users.noreply.github.com>
2026-01-03 23:17:15 +0200

    update

--- variable.tf
+++ variable.tf
@@ -5,5 +5,21 @@ variable "kubeconfig_path" {
   default     = "~/.kube/config"
 }
 
+variable "crossplane_namespace" {
+  type = string
+}
+
+variable "crossplane_version" {
+  type = string
+}
+
+variable "crossplane_replicas" {
+  type = number
+}
+
+variable "crossplane_metrics_enabled" {
+  type = bool
+}
+
 
 
