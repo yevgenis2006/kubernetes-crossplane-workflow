113fd20ad90a024775e7e13cf93cc6eb26a625f4
Yevgeni Shapiro <23049337+yevgenis2006@users.noreply.github.com>
2026-01-03 23:46:27 +0200

    update

--- config/gcp/instance.tf
+++ config/gcp/instance.tf
@@ -1,1 +1,51 @@
 
+resource "kubernetes_manifest" "provider_gcp" {
+  manifest = {
+    apiVersion = "pkg.crossplane.io/v1"
+    kind       = "Provider"
+    metadata = {
+      name = "provider-gcp"
+    }
+    spec = {
+      package = "xpkg.upbound.io/crossplane-contrib/provider-gcp:v0.25.0"
+    }
+  }
+}
+
+resource "kubernetes_secret" "gcp_creds" {
+  metadata {
+    name      = "gcp-creds"
+    namespace = "crossplane-system"
+  }
+
+  data = {
+    key = base64encode(file(var.gcp_sa_key_file))
+  }
+}
+
+resource "kubernetes_manifest" "provider_config_gcp" {
+  manifest = {
+    apiVersion = "gcp.crossplane.io/v1beta1"
+    kind       = "ProviderConfig"
+    metadata = {
+      name = "gcp-default"
+    }
+    spec = {
+      credentials = {
+        source = "Secret"
+        secretRef = {
+          namespace = "crossplane-system"
+          name      = kubernetes_secret.gcp_creds.metadata[0].name
+          key       = "key"
+        }
+      }
+    }
+  }
+
+  depends_on = [kubernetes_manifest.provider_gcp]
+}
+
+variable "gcp_sa_key_file" {
+  description = "Path to GCP Service Account JSON"
+}
+
